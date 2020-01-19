66ebb3cd3f7474a525407bfa666186c581bdaece
Jalastin <57930712+Jalastin@users.noreply.github.com>
2020-01-18 23:20:04 -0800

    kmeans with a fixed graph

--- kmeans.py
+++ kmeans.py
@@ -7,6 +7,10 @@ kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
 print(kmeans.labels_)
 print(kmeans.cluster_centers_)
 
+labels = kmeans.labels_
+dcolor = {1:"red", 0:"green"}
+for idx, val in enumerate(X):
+    plt.scatter(val[0], val[1], color=dcolor[labels[idx]])
 centers = kmeans.cluster_centers_
-plt.scatter(centers[:, 0], centers[:1], c='black')
+plt.scatter(centers[:, 0], centers[:, 1], c='black', marker='x', linewidths=3)
 plt.show()
